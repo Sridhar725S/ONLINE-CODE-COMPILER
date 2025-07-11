6c4c44aaf2ebfb496dcd9062dd3c862d0275f01e
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-07-11 20:53:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const { exec } = require('child_process');
 const cors = require('cors');
-const path = require('path');
 const app = express();
 const PORT = 3000;
 
